15c65325489ef9266267027dbca2adde10ea859a
Brandon Swanson Ubuntu <swanyriver@gmail.com>
2014-12-12 18:44:54 -0800

    second attempt

--- newprob3.py
+++ newprob3.py
@@ -1,27 +1,26 @@
+import math
+
 BIGNUM = 600851475143
 largest_possibe_factor = BIGNUM/2
 largest = 0;
 
-primes = [2,3]
 
 def isPrime(x):
-    for a in primes:
-        if not x%a:
+    if not x%3:
+        return False
+    r = int(math.sqrt(x))
+    f = 5
+    while f<=r:
+        if not x%f or not x%(f+2):
             return False
+        f+=6
     return True
 
-scaleFactor = 10000
-scale = 0
-
-for b in xrange(4,largest_possibe_factor):
+for b in xrange(11,largest_possibe_factor):
+    if not b%10000:
+        print largest_possibe_factor-b, " remaining"
     if isPrime(b):
-        primes.append(b)
-        scale += 1
-        if(scale == scaleFactor):
-            scale = 0
-            print "new prime: ", b, round(float(b/float(largest_possibe_factor)),5)
         if not BIGNUM%b:
-            print " isfactor"
             largest = b
 
-print "larges factor is: ", largest
+print "largest factor is: ", largest
