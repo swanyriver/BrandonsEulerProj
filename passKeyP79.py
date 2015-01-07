1ca368188a0a11ec3fdc1e4f8c0c19f9b3427087
Brandon Swanson Ubuntu <swanyriver@gmail.com>
2015-01-06 21:08:31 -0800

    passphrase was correct, i was answer what was its length, not what was it

--- passKeyP79.py
+++ passKeyP79.py
@@ -1,37 +1,40 @@
+import brandonsEP
+
 f = open("keylog.txt")
 keys = []
 for line in f:
     key = []
     for c in line[:-1]: key.append(int(c))
     keys.append(key)
+f.close()
 
-#print keys
-#print any(keys)
 
-keyphraseLength=0
+
 keyphrase = 0
 counts = [0]*10
 for key in keys:
     counts[key[0]]+=1
 
-#print counts.index(max(counts))
+##print counts.index(max(counts))
+#print "first count ", counts
 
 while any(keys):
     next = counts.index(max(counts))
     keyphrase*=10
     keyphrase+=next
     counts[next]=0
-    keyphraseLength+=1
     for key in keys:
-        print key
+        #print key
 
         if not key or key[0]!=next:continue
         del key[0]
         if key: counts[key[0]]+=1
         
-    print next, counts
+    #print next, counts
 
 print keyphrase
-print keyphraseLength
+#print brandonsEP.lengthNum(keyphrase)
+
+
 
     
